4134e03d031d8b5df5b968d5654f27f1eb414938
tin man <155429519+tinmanlab@users.noreply.github.com>
2024-01-17 20:30:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Compatible android app
+# Compatible Android app
 # https://play.google.com/store/apps/details?id=com.dev47apps.obsdroidcam
 
 import cv2
@@ -7,11 +7,10 @@ from QuickCaptureModule import Capture
 
 detector = HandDetector(maxHands=1, detectionCon=0.5, minTrackCon=0.5)
 
-# Use TCP/IP address of your mobile phone
-#cap = Capture('http://192.168.1.86:4747/video')
-#cap = Capture('http://192.168.43.162:4747/video')
+# Use the TCP/IP address of your mobile phone
+#cap = Capture('http://192.168.?.?:?/video')
 
-# Default camera view in your PC
+# Default camera view on your PC
 cap = Capture()
 
 if __name__ == "__main__":
@@ -20,9 +19,10 @@ if __name__ == "__main__":
         frame = cap.read()
 
         # Process frame
-        rotated_img = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        #rotated_img = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
         #hands, img = detector.findHands(cv2.flip(rotated_img, 1))
-        hands, img = detector.findHands(rotated_img)
+        
+        hands, img = detector.findHands(frame)
 
         cv2.imshow('', img)
 
